533742be292f660c4448d5ac01fd628fa8d9cdf0
Pablo Fiallos <159185130+pablo-fiallos-granados@users.noreply.github.com>
2024-04-03 18:28:39 +0000

    everything

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Pablo Fiallos
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+
+function enterClicked() {
+  // input
+  const l = parseFloat(document.getElementById("lValue").value)
+  const w = parseFloat(document.getElementById("wValue").value)
+  const h = parseFloat(document.getElementById("hValue").value)
+  const volume = (l * w * h) / 3
+  const volumeRounded = volume.toFixed(2)
+  // output
+  document.getElementById("user-info").innerHTML =
+    "A = " + volumeRounded + "u <sup>3</sup>"
+}
